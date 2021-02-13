2d71deaf5a7ce2b96136e8b42f4a1f3fd327db13
wulan17 <galihgustip@gmail.com>
2021-02-14 01:54:39 +0700

    check if given youtube link is valid or not
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- src/handlers/play.ts
+++ src/handlers/play.ts
@@ -12,12 +12,20 @@ export const playHandler = Composer.command('play', async ctx => {
     const [commandEntity] = ctx.message.entities!;
     const text = ctx.message.text.slice(commandEntity.length + 1);
 
+    var re = new RegExp("^https://www.youtube.com/([a-zA-Z0-9])");
+    var re2 = new RegExp("^https://youtube.com/([a-zA-Z0-9])");
+    var re3 = new RegExp("^https://www.youtu.be/([a-zA-Z0-9])");
+    var re4 = new RegExp("^https://youtu.be/([a-zA-Z0-9])");
     if (!text) {
         await ctx.reply('You need to specify a YouTube URL.');
         return;
+    } else if (re.test(text) || re2.test(text) || re3.test(text) || re4.test(text)) {
+	    const index = await addToQueue(chat, text);
+
+    	await ctx.reply(index === 0 ? 'Playing.' : `Queued at position ${index}.`);
+
+    } else {
+        await ctx.reply('Give a valid YouTube URL.');
+        return;
     }
-
-    const index = await addToQueue(chat, text);
-
-    await ctx.reply(index === 0 ? 'Playing.' : `Queued at position ${index}.`);
 });
